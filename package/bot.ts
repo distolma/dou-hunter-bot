9dd4e250f671225782339992856ac159916da824
distolma <djmakaron2009@gmail.com>
2018-04-02 09:26:14 +0300

    running worker

--- package/bot.ts
+++ package/bot.ts
@@ -1,13 +1,12 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-const { BOT_TOKEN, NODE_ENV } = process.env;
-const url = process.env.APP_URL;
-export let bot: TelegramBot;
+const { BOT_TOKEN } = process.env;
+// const url = process.env.APP_URL;
+export let bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
-if (NODE_ENV === 'development') {
-  bot = new TelegramBot(BOT_TOKEN, { polling: true });
-} else {
-  bot = new TelegramBot(BOT_TOKEN);
+// if (NODE_ENV === 'development') {
+// } else {
+//   bot = new TelegramBot(BOT_TOKEN);
 
-  bot.setWebHook(`${url}/bot${BOT_TOKEN}`);
-}
+//   bot.setWebHook(`${url}/bot${BOT_TOKEN}`);
+// }
