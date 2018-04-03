776b095e7b47da68597cfd46628bf5ea754ca17f
distolma <djmakaron2009@gmail.com>
2018-04-03 22:19:58 +0300

    changed api methods

--- package/utils/api.ts
+++ package/utils/api.ts
@@ -6,7 +6,7 @@ import { IDOUTokens, IDOUParams, IDOUXHRResponse } from '../interfaces';
 
 const { SOURCE_URL, XHR_URL } = process.env;
 
-const getTokens = () =>
+export const getTokens = () =>
   axios.get<string>(SOURCE_URL).then<IDOUTokens>(response => {
     const { csrftoken } = cookie.parse(response.headers['set-cookie'][0]);
     const csrfmiddlewaretoken = getCSRFMiddlewareToken(response.data);
@@ -14,22 +14,27 @@ const getTokens = () =>
     return { csrftoken, csrfmiddlewaretoken };
   });
 
-const getVacanciesTemplate = (params: IDOUParams) => (tokens: IDOUTokens) =>
-  axios.post<IDOUXHRResponse>(
-    XHR_URL,
-    `csrfmiddlewaretoken=${tokens.csrfmiddlewaretoken}`,
-    {
-      params,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Referer: SOURCE_URL,
-        Cookie: cookie.serialize('csrftoken', tokens.csrftoken),
+export const getVacanciesTemplate = (tokens: IDOUTokens) => (
+  params: IDOUParams,
+) =>
+  axios
+    .post<IDOUXHRResponse>(
+      XHR_URL,
+      `csrfmiddlewaretoken=${tokens.csrfmiddlewaretoken}`,
+      {
+        params,
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Referer: SOURCE_URL,
+          Cookie: cookie.serialize('csrftoken', tokens.csrftoken),
+        },
       },
-    },
-  );
-
-export const getVacancies = (params: IDOUParams) =>
-  getTokens()
-    .then(getVacanciesTemplate(params))
+    )
     .then(response => response.data.html)
     .then(getVacancyList);
+
+// export const getVacancies = (params: IDOUParams) =>
+//   getTokens()
+//     .then(getVacanciesTemplate(params))
+//     .then(response => response.data.html)
+//     .then(getVacancyList);
