a65e36a653ceaf652dbac977a6ad361d74593a64
Dmitry Mostovoy <dmitry.mostovoy@idealscorp.com>
2018-04-03 18:43:21 +0300

    added new interfaces

--- package/interfaces/api.ts
+++ package/interfaces/api.ts
@@ -1,30 +1,46 @@
-export interface DouRSS {
-  rss: {
-    channel: {
-      description: string;
-      item: Vacancy[];
-      language: string;
-      lastBuildDate: string;
-      link: string;
-      title: string;
-    };
-  };
-}
-
-export interface Vacancy {
-  description: string;
-  guid: string;
-  link: string;
-  pubDate: string;
-  title: string;
-}
-
-export interface HtmlData {
+export interface IVacancy {
   id: number;
   title: string;
   company: string;
   cities: string[];
   description: string;
   hot: boolean;
-  url?: string;
+  url: string;
+}
+
+export interface IDOUTokens {
+  csrftoken: string;
+  csrfmiddlewaretoken: string;
+}
+
+export interface IDOUXHRResponse {
+  html: string;
+  last: boolean;
+  num: number;
+}
+
+export type IDOUParams =
+  | IDOURelocationParams
+  | IDOUBeginnersParams
+  | IDOURemoteParams
+  | IDOUCityParams;
+
+interface IDOUCityParams {
+  city: string;
+  category: string;
+}
+
+interface IDOURelocationParams {
+  relocation: boolean;
+  category: string;
+}
+
+interface IDOUBeginnersParams {
+  beginners: boolean;
+  category: string;
+}
+
+interface IDOURemoteParams {
+  remote: boolean;
+  category: string;
 }
--- package/interfaces/index.ts
+++ package/interfaces/index.ts
@@ -1,9 +1,1 @@
 export * from './api';
-
-export interface UserVacancies {
-  category?: string;
-  city?: string;
-  relocation?: boolean;
-  beginners?: boolean;
-  remote?: boolean;
-}
