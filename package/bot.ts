0945d2269537e19523e822eb22b4fff67f0dc740
Dmitry Mostovoy <dmitry.mostovoy@idealscorp.com>
2018-04-03 18:44:32 +0300

    removed commented codes

--- package/bot.ts
+++ package/bot.ts
@@ -1,12 +1,5 @@
 import TelegramBot from 'node-telegram-bot-api';
 
 const { BOT_TOKEN } = process.env;
-// const url = process.env.APP_URL;
+
 export let bot = new TelegramBot(BOT_TOKEN, { polling: true });
-
-// if (NODE_ENV === 'development') {
-// } else {
-//   bot = new TelegramBot(BOT_TOKEN);
-
-//   bot.setWebHook(`${url}/bot${BOT_TOKEN}`);
-// }
