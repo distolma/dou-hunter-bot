ebdf488a99cdfc25ef54390467fabb8f6171578a
Dmytro Mostovyi <djmakaron2009@gmail.com>
2020-02-27 18:06:58 +0200

    :zap: Remove built in cron

--- package/app.ts
+++ package/app.ts
@@ -3,7 +3,6 @@ import bodyParser from 'koa-bodyparser';
 import { AddressInfo } from 'net';
 
 import { routes } from './routes/app';
-import { cron } from './cron';
 import './routes/bot';
 
 const { PORT } = process.env;
@@ -15,8 +14,6 @@ app.use(routes);
 const server = app.listen(PORT || 80, () => {
   const { address, port } = server.address() as AddressInfo;
   console.log(`Web server started at ${address}:${port}`);
-
-  cron();
 });
 
 process.on('unhandledRejection', reason => {
